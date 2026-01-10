2bedb668dc83b949f2e8aaf8be17138b0f582cd0
FilipaPinto04 <filipamoraispinto2004@gmail.com>
2026-01-10 10:20:22 +0000

    ddddddddddd

--- projeto/Agents/plataforma_agent.py
+++ projeto/Agents/plataforma_agent.py
@@ -2,17 +2,12 @@ from spade.agent import Agent
 from Behaviour.cyclic_plataforma import CyclicBehavPlataforma
 from Behaviour.periodic_plataforma import PeriodicBehavPlataforma
 
-<<<<<<< HEAD
-class CustomerAgent(Agent):
+class PlataformaAgent(Agent):
 
     paciente_subscribe = []
     medico_subscribe = []
     historico_falhas = {}
-=======
-class PlataformaAgent(Agent):
-    historico_falhas = {}
 
->>>>>>> 9c69ac535f643e34c35bdc31af9d5708352ed5d8
     async def setup(self):
         print(f"agente customer iniciado: {self.jid}")
         a = CyclicBehavPlataforma()
